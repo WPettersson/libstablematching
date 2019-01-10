f4219f8e2216edb93ae0340511a6b26f48c52bde
William Pettersson <william@ewpettersson.se>
2019-01-10 12:54:58 +0000

    Add test instance and new main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,20 @@
 int main(int argc, char *argv[]) {
   int seed = 24601;
   std::mt19937 g(seed);
-  SMTI instance(2, 2, 0.15, g);
+  SMTI instance(10, 5, 0.75, g);
   std::cout << instance.to_string();
-  std::cout << instance.encodeSAT();
+  instance.add_dummy(1);
+  std::cout << instance.to_string();
+  instance.add_dummy(3);
+  std::cout << instance.to_string();
+  instance.remove_dummy(2);
+  std::cout << instance.to_string();
+  instance.remove_dummy(2);
+  std::cout << instance.to_string();
+  std::cout << instance.encodeWPMaxSAT();
+  SMTI instance_two("test.instance");
+  std::cout << instance_two.to_string();
+  std::cout << instance_two.encodeSAT();
+  std::cout << instance_two.encodeWPMaxSAT();
   return 0;
 }
