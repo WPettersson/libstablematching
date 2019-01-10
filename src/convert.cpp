a83858dbd845d0b92a45391370dfc019ab1052df
William Pettersson <william@ewpettersson.se>
2019-01-10 16:41:35 +0000

    convert: can add dummy agents for SAT instances
    convert now supports a -d option to add a number of dummy agents. The
    assumption is that if you are adding dummy agents, you want to make an
    instance of SAT, not MaxSAT.

--- src/convert.cpp
+++ src/convert.cpp
@@ -8,7 +8,14 @@ int main(int argc, char *argv[]) {
     std::cout << "Usage: convert <instance_file>" << std::endl;
     return 1;
   }
-  SMTI instance(argv[1]);
-  std::cout << instance.encodeWPMaxSAT();
+  if (std::string("-d") == argv[1]) {
+    int dummies = std::atoi(argv[2]);
+    SMTI instance(argv[3]);
+    instance.add_dummy(dummies);
+    std::cout << instance.encodeSAT();
+  } else {
+    SMTI instance(argv[1]);
+    std::cout << instance.encodeWPMaxSAT();
+  }
   return 0;
 }
