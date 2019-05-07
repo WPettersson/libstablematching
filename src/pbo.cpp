aaa291c923966498890c6592ff7f7b87a6b72c3b
William Pettersson <william.pettersson@glasgow.ac.uk>
2019-05-07 17:48:02 +0100

    Add program to create PBO file

---
+++ src/pbo.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <random>
+
+#include "smti.h"
+
+int main(int argc, char *argv[]) {
+  //int seed = 24601;
+  //std::mt19937 g(seed);
+  std::random_device rd;
+  std::mt19937 g(rd());
+  SMTI instance(atoi(argv[1]), atoi(argv[2]), atof(argv[3]), g);
+  std::string fname(argv[4]);
+  std::ofstream humanfile(fname + ".instance");
+  humanfile << instance.to_string();
+  humanfile.close();
+  std::ofstream wcnffile(fname + ".pbo");
+  wcnffile << instance.encodePBO();
+  wcnffile.close();
+  return 0;
+}
