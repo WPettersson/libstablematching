9412332737ecb9e0f72d7bcafbf6a2a5fc16afc4
William Pettersson <william.pettersson@glasgow.ac.uk>
2019-08-05 16:37:23 +0100

    Tweaks to converter programs

--- src/pbo_convert.cpp
+++ src/pbo_convert.cpp
@@ -7,16 +7,12 @@
 
 int main(int argc, char *argv[]) {
   if (argc != 3) {
-    std::cout << "Usage: convert_pbo <instance_file> <prefix>" << std::endl;
+    std::cout << "Usage: convert_pbo <instance_file> <output_file>" << std::endl;
     return 1;
   }
   SMTI instance(argv[1]);
-  std::ofstream wcnffile2(std::string(argv[2]) + "-v2.pbo");
+  std::ofstream wcnffile2(argv[2]);
   wcnffile2 << instance.encodePBO2();
   wcnffile2.close();
-#ifdef CPLEX_FOUND
-  double size = instance.solve_cplex();
-  std::cout << "CPLEX found " << size << std::endl;
-#endif
   return 0;
 }
